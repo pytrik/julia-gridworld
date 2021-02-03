fece4a9cc981bd919479a3a782f982445420689e
leazael <leazael@gmail.com>
2021-02-03 21:00:09 +0100

    It learned a thing!

--- gridworld_v01.jl
+++ gridworld_v01.jl
@@ -31,9 +31,8 @@ state2coordinates(state::Int64) = ( (state-1) % worldWidth , (state-1) ÷ worldW
 function show_state_as_world(world::ASCanvas, state::Int64)
     coords = state2coordinates(state)
     hero = Sprite("wall","@"; foreground = :blue, background = :none, x = coords[1] * 2, y = coords[2])
-    push!(world.stack, hero)
-    print(world)
-    pop!(world.stack)
+    tempWorld = ASCanvas(world.width, world.height, [world.stack; hero])
+    print(tempWorld)
 end
 
 function can_move(state::Int64, action::Action)
@@ -58,13 +57,47 @@ function can_move(state::Int64, action::Action)
 end
 
 list_feasible_actions(state::Int64) = actions[map(act -> can_move(state, act), actions)]
+enact(state::Int64, action::Action) = coordinates2state( Tuple([state2coordinates(state)...] + [action.move...])... )
+enact(state::Int64, actionIndex::Int64) = enact(state, actions[actionIndex])
 
-s = 96
-show_state_as_world(world, s);
-list_feasible_actions(s)
+const stateActionMatrix = [enact(j,k) for j in 1:nState, k in 1:4]
+const startState = 1
+const endState = coordinates2state(6,6)
 
-# list_feasible_actions
+reward(targetState::Int64) = targetState == endState ? 1 : 0
 
-# apply_state
+# ΔQ[ S[t], A[t] ] = α ( R[t+1] + γ max_a{ Q[ S[t+1], a ]} - Q[ S[t], A[t] ] )
+function updateQ!(Q::Array{Float64,2}, α::Float64, γ::Float64, currentState::Int64, actionIndex::Int64)
+    nextState = enact(currentState, actionIndex)
+    nextReward = reward(nextState)
+    expectedFutureReward = γ * max(Q[nextState,:]...)
+    Q[currentState, actionIndex] = Q[currentState, actionIndex] + α * ( nextReward + expectedFutureReward - Q[currentState, actionIndex])
+    return nextState
+end 
 
-# lol, dit is een commpent
+learningPolicy(state::Int64) = rand(list_feasible_actions(state)).index
+
+Q = zeros(Float64, nState, length(actions))
+state = startState;
+
+epochs = 0
+
+for k=1:1000000
+    state = updateQ!(Q, 0.1, 0.9, state, learningPolicy(state))
+    if state == endState
+        state = startState
+        epochs +=1 
+    end
+end
+
+function bestPolicy(state::Int64)
+    feasActIndex = map(x->x.index, list_feasible_actions(state))
+    value, ind = findmax(Q[state,feasActIndex])
+    return feasActIndex[ind]
+end
+
+state = startState
+show_state_as_world(world, state)
+
+state = enact(state, bestPolicy(state))
+show_state_as_world(world, state)
