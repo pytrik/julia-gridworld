3e86ab2c1d19f0e64444dcf05cbfa27c2b705837
Pytrik Elzinga <pytrik.elzinga@gmail.com>
2021-02-10 22:00:07 +0100

    Some refactoring and trying epsilon-greedy policy

--- gridworld_v01.jl
+++ gridworld_v01.jl
@@ -1,6 +1,7 @@
 push!(LOAD_PATH, pwd());
 using Revise
 using ASCIICanvas
+using DelimitedFiles
 
 background = load_sprite("achtergrond", "map.sprite")
 const wallCoordinates = [[(2, j) for j in 0:4]; [(5, j) for j in 3:7]; [(j, 7) for j in 5:10]]
@@ -64,40 +65,59 @@ const stateActionMatrix = [enact(j,k) for j in 1:nState, k in 1:4]
 const startState = 1
 const endState = coordinates2state(6,6)
 
-reward(targetState::Int64) = targetState == endState ? 1 : 0
+reward(targetState::Int64) = targetState == endState ? 100 : -1
 
 # ΔQ[ S[t], A[t] ] = α ( R[t+1] + γ max_a{ Q[ S[t+1], a ]} - Q[ S[t], A[t] ] )
 function updateQ!(Q::Array{Float64,2}, α::Float64, γ::Float64, currentState::Int64, actionIndex::Int64)
     nextState = enact(currentState, actionIndex)
     nextReward = reward(nextState)
-    expectedFutureReward = γ * max(Q[nextState,:]...)
-    Q[currentState, actionIndex] = Q[currentState, actionIndex] + α * ( nextReward + expectedFutureReward - Q[currentState, actionIndex])
+    feasibleActions = map(a -> a.index, list_feasible_actions(nextState))
+    expectedFutureReward = γ * max(Q[nextState, feasibleActions]...)
+    ΔQ = α * ( nextReward + expectedFutureReward - Q[currentState, actionIndex])
+    Q[currentState, actionIndex] = Q[currentState, actionIndex] + ΔQ
     return nextState
 end 
 
-learningPolicy(state::Int64) = rand(list_feasible_actions(state)).index
+function bestPolicy(Q::Array{Float64, 2}, state::Int64)
+    feasActIndex = map(x->x.index, list_feasible_actions(state))
+    value, ind = findmax(Q[state,feasActIndex])
+    return feasActIndex[ind]
+end
+
+randomPolicy(Q::Array{Float64, 2}, state::Int64) = rand(list_feasible_actions(state)).index
+
+function learningPolicy(Q::Array{Float64, 2}, state::Int64, epochs::Int64)
+    if rand() < exp(-epochs)
+        return randomPolicy(Q, state)
+    else
+        return bestPolicy(Q, state)
+    end
+end
 
 Q = zeros(Float64, nState, length(actions))
 state = startState;
 
 epochs = 0
 
-for k=1:1000000
-    state = updateQ!(Q, 0.1, 0.9, state, learningPolicy(state))
+for k=1:10000
+    state = updateQ!(Q, 0.1, 0.9, state, learningPolicy(Q, state, epochs))
     if state == endState
         state = startState
-        epochs +=1 
+        epochs += 1 
     end
 end
 
-function bestPolicy(state::Int64)
-    feasActIndex = map(x->x.index, list_feasible_actions(state))
-    value, ind = findmax(Q[state,feasActIndex])
-    return feasActIndex[ind]
+# het is niet nodig Q te herberekenen voor veranderde startState
+state = 1
+show_state_as_world(world, state)
+
+for k = 1:18
+    state = enact(state, bestPolicy(Q, state))
+end
+show_state_as_world(world, state)
+
+open("out.csv", "w") do io
+    writedlm(io, Q)
 end
 
-state = startState
-show_state_as_world(world, state)
-
-state = enact(state, bestPolicy(state))
-show_state_as_world(world, state)
+print("nr of epochs: ", epochs)
